2c1bb886618485ec9bcb7bf39e91e94415bbfe5d
Salvator <salvator.galea@cl.cam.ac.uk>
2017-05-11 15:51:52 +0100

    Migrate reference_switch_lite project to Vivado 2016.4

--- projects/reference_switch_lite/hw/create_ip/nf_10ge_interface.tcl
+++ projects/reference_switch_lite/hw/create_ip/nf_10ge_interface.tcl
@@ -1,5 +1,6 @@
 #
 # Copyright (c) 2015 University of Cambridge
+# Modified by Salvator Galea
 # All rights reserved.
 #
 # This software was developed by Stanford University and the University of Cambridge Computer Laboratory 
@@ -29,10 +30,9 @@
 # Set variables
 
 # CORE CONFIG parameters
-set sharedLogic          "FALSE"
-set tdataWidth           256
-
-set convWidth [expr $tdataWidth/8]
+set sharedLogic		"FALSE"
+set tdataWidth		256
+set convWidth		[expr $tdataWidth/8]
 
 if { $sharedLogic eq "True" || $sharedLogic eq "TRUE" || $sharedLogic eq "true" } {
    set supportLevel 1
@@ -40,7 +40,7 @@ if { $sharedLogic eq "True" || $sharedLogic eq "TRUE" || $sharedLogic eq "true" 
    set supportLevel 0
 }
 
-create_ip -name axi_10g_ethernet -vendor xilinx.com -library ip -version 2.0 -module_name axi_10g_ethernet_nonshared
+create_ip -name axi_10g_ethernet -vendor xilinx.com -library ip -version 3.1 -module_name axi_10g_ethernet_nonshared
 set_property -dict [list CONFIG.Management_Interface {false}] [get_ips axi_10g_ethernet_nonshared]
 set_property -dict [list CONFIG.base_kr {BASE-R}] [get_ips axi_10g_ethernet_nonshared]
 set_property -dict [list CONFIG.SupportLevel $supportLevel] [get_ips axi_10g_ethernet_nonshared]
@@ -53,7 +53,7 @@ reset_target all [get_ips axi_10g_ethernet_nonshared]
 generate_target all [get_ips axi_10g_ethernet_nonshared]
 
 
-create_ip -name fifo_generator -vendor xilinx.com -library ip -version 12.0 -module_name fifo_generator_status
+create_ip -name fifo_generator -vendor xilinx.com -library ip -version 13.1 -module_name fifo_generator_status
 set_property -dict [list CONFIG.Fifo_Implementation {Independent_Clocks_Block_RAM}] [get_ips fifo_generator_status]
 set_property -dict [list CONFIG.Performance_Options {First_Word_Fall_Through}] [get_ips fifo_generator_status]
 set_property -dict [list CONFIG.Input_Data_Width {458} CONFIG.Input_Depth {16}] [get_ips fifo_generator_status]
@@ -81,12 +81,9 @@ set_property generate_synth_checkpoint false [get_files inverter_0.xci]
 reset_target all [get_ips inverter_0]
 generate_target all [get_ips inverter_0]
 
-create_ip -name fifo_generator -vendor xilinx.com -library ip -version 12.0 -module_name fifo_generator_1_9
+create_ip -name fifo_generator -vendor xilinx.com -library ip -version 13.1 -module_name fifo_generator_1_9
 set_property -dict [list CONFIG.Fifo_Implementation {Independent_Clocks_Block_RAM} CONFIG.Performance_Options {First_Word_Fall_Through} CONFIG.Input_Data_Width {1} CONFIG.Input_Depth {16} CONFIG.Output_Data_Width {1} CONFIG.Output_Depth {16} CONFIG.Data_Count_Width {4} CONFIG.Write_Data_Count_Width {4} CONFIG.Read_Data_Count_Width {4} CONFIG.Full_Threshold_Assert_Value {13} CONFIG.Full_Threshold_Negate_Value {12}] [get_ips fifo_generator_1_9]
 
 set_property generate_synth_checkpoint false [get_files fifo_generator_1_9.xci]
 reset_target all [get_ips fifo_generator_1_9]
 generate_target all [get_ips fifo_generator_1_9]
-
-
-
