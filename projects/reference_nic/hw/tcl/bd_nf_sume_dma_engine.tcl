02b8dd3679a9ae2eb37c7f460678fa5e8f8424ed
Marcin Wójcik <marcin.wojcik.mw@gmail.com>
2020-08-18 18:38:13 +0100

    Change rst signal in the async data fifo
    The newest version of async data fifo has only one
    rst attached. No longer things can be sync to one of
    the sides. The pipeline rst has been attached instead
    of pcie rst. This change removes reset inverter
    module too.

--- projects/reference_nic/hw/tcl/bd_nf_sume_dma_engine.tcl
+++ projects/reference_nic/hw/tcl/bd_nf_sume_dma_engine.tcl
@@ -102,13 +102,8 @@ proc create_hier_cell_nf_sume_dma_engine { parentCell coreName tdataWidth } {
    set_property -dict [list CONFIG.C_SIZE {1}] [get_bd_cells pcie_inverter_0]
    set_property -dict [list CONFIG.C_OPERATION {not}] [get_bd_cells pcie_inverter_0]
    
-   # pcie user clock reset inverter.
-   create_bd_cell -type ip -vlnv xilinx.com:ip:util_vector_logic:2.0 user_pcie_inverter_0
-   set_property -dict [list CONFIG.C_SIZE {1}] [get_bd_cells user_pcie_inverter_0]
-   set_property -dict [list CONFIG.C_OPERATION {not}] [get_bd_cells user_pcie_inverter_0]
-
    # load pcie endpoint.
-   create_bd_cell -type ip -vlnv xilinx.com:ip:pcie3_7x:4.2 pcie3_7x_1
+   create_bd_cell -type ip -vlnv xilinx.com:ip:pcie3_7x:4.3 pcie3_7x_1
    set_property -dict [list CONFIG.xlnx_ref_board {None}] [get_bd_cells pcie3_7x_1]
    set_property -dict [list CONFIG.pcie_blk_locn {X0Y1} CONFIG.PL_LINK_CAP_MAX_LINK_WIDTH {X8}] [get_bd_cells pcie3_7x_1]
    set_property -dict [list CONFIG.PL_LINK_CAP_MAX_LINK_SPEED {5.0_GT/s} CONFIG.axisten_if_width {128_bit}] [get_bd_cells pcie3_7x_1]
@@ -177,7 +172,7 @@ proc create_hier_cell_nf_sume_dma_engine { parentCell coreName tdataWidth } {
    connect_bd_net [get_bd_pins axis_aclk] [get_bd_pins axis_data_fifo_0/m_axis_aclk]
    connect_bd_net [get_bd_pins axis_aclk] [get_bd_pins axis_data_fifo_1/s_axis_aclk]
    
-   connect_bd_net [get_bd_pins axis_aresetn] [get_bd_pins axis_data_fifo_0/m_axis_aresetn]
+   connect_bd_net [get_bd_pins axis_aresetn] [get_bd_pins axis_data_fifo_0/s_axis_aresetn]
    connect_bd_net [get_bd_pins axis_aresetn] [get_bd_pins axis_data_fifo_1/s_axis_aresetn]
    
    if { $tdataWidth ne "128" } {
@@ -197,7 +192,6 @@ proc create_hier_cell_nf_sume_dma_engine { parentCell coreName tdataWidth } {
    
    connect_bd_net [get_bd_pins pcie3_7x_1/user_clk] [get_bd_pins nf_riffa_dma_0/user_clk]
    connect_bd_net [get_bd_pins pcie3_7x_1/user_reset] [get_bd_pins nf_riffa_dma_0/user_reset]
-   connect_bd_net [get_bd_pins pcie3_7x_1/user_reset] [get_bd_pins user_pcie_inverter_0/Op1]
    connect_bd_net [get_bd_pins pcie3_7x_1/user_lnk_up] [get_bd_pins nf_riffa_dma_0/user_lnk_up]
    
    connect_bd_intf_net [get_bd_intf_pins pcie3_7x_1/m_axis_cq] [get_bd_intf_pins nf_riffa_dma_0/m_axis_cq]
@@ -271,9 +265,6 @@ proc create_hier_cell_nf_sume_dma_engine { parentCell coreName tdataWidth } {
    
    connect_bd_net [get_bd_pins pcie3_7x_1/user_clk] [get_bd_pins axis_data_fifo_0/s_axis_aclk]
    connect_bd_net [get_bd_pins pcie3_7x_1/user_clk] [get_bd_pins axis_data_fifo_1/m_axis_aclk]
-   
-   connect_bd_net [get_bd_pins user_pcie_inverter_0/Res] [get_bd_pins axis_data_fifo_0/s_axis_aresetn]
-   connect_bd_net [get_bd_pins user_pcie_inverter_0/Res] [get_bd_pins axis_data_fifo_1/m_axis_aresetn]
    
    if { $tdataWidth ne "128" } {
       connect_bd_intf_net [get_bd_intf_pins axis_data_fifo_0/M_AXIS] [get_bd_intf_pins axis_dwidth_converter_0/S_AXIS]
